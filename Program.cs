b8613a996b8e7ae6b3114ba405306adf44bccdc6
Kamran <muskam10@mail.ru>
2023-03-30 19:11:43 +0400

    Final Task1

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача:
+// писать программу, которая из имеющегося массива строк формирует массив из строк, 
+// длина которых меньше либо равна 3 символам.
+// Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма
+// При решении не рекомндуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+string[] GetArrayThreeSimvol (string[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        string simvol = array[i];
+        if (simvol.Length <= 3)
+        {
+            count++;
+        }
+    }
+    string[] arrayThreeSimvol = new string[count];
+    for (int i = 0, j = 0; i < array.Length; i++)
+    {
+        string simvol = array[i];
+        if (simvol.Length <= 3)
+        {
+            arrayThreeSimvol[j] = simvol;
+            j++;
+        }
+    }
+    return arrayThreeSimvol;
+}
